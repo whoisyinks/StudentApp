74d0c634b8a4e27173d8a51945cfbb079d028ece
user <user@IYINKS>
2024-07-20 15:24:01 +0000

    Add project files.

---
+++ Migrations/20240706213042_initdata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class initdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdmissionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public string? Department { get; set; }
+        public decimal Price { get; set; }
+        public string? City { get; set; }
+    }
+}
